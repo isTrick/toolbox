79560b5309141ba1831bbfde327edf2127cda1d6
Patrick Girardi <dev.patrickgirardi@gmail.com>
2024-11-01 14:46:39 -0300

    chore: project organization

--- src/app/photo-enhancer/page.tsx
+++ src/app/photo-enhancer/page.tsx
@@ -1,4 +1,20 @@
-export default function PhotoEnhancer() {
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+import { redirect, RedirectType } from 'next/navigation'
+
+export default async function PhotoEnhancer() {
+  let loggedIn = false
+
+  try {
+    const supabase = createServerComponentClient( {cookies} )
+    const { data: { session } } = await supabase.auth.getSession()
+
+    if (session) loggedIn = true;
+  } catch(error) {
+    console.log("Home", error)
+  } finally {
+    if (loggedIn) redirect("/", RedirectType.replace)
+  }
 
   return ( <div className="bg-green-400 w-screen h-screen"></div> )
 } 
